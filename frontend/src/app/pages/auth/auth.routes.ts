422920e70cdccf52086c24425b228a1e9d2db516
Amilcar <amilkargm@proton.me>
2025-08-13 11:25:58 -0600

    Adjust routes and layouts for a simpler UX

--- frontend/src/app/pages/auth/auth.routes.ts
+++ frontend/src/app/pages/auth/auth.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
-import { Access } from './access';
 import { Login } from './login';
-import { Error } from './error';
 
-export default [
-    { path: 'access', component: Access },
-    { path: 'error', component: Error },
-    { path: 'login', component: Login }
-] as Routes;
+export const authRoutes: Routes = [
+    {
+        path: '',
+        component: Login,
+        children: [
+            {
+                path: 'login',
+                component: Login
+            }
+        ]
+    }
+];
+
+export default authRoutes;
