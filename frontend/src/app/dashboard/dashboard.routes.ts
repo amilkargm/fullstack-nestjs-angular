5c50998e202e7ab29c560e8e5abcc846cdfd650c
Amilcar <amilkargm@proton.me>
2025-08-13 20:48:34 -0600

    Add Products module, services and routes to frontend

--- frontend/src/app/dashboard/dashboard.routes.ts
+++ frontend/src/app/dashboard/dashboard.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { DashboardLayoutComponent } from './layout/dashboard-layout/dashboard-layout.component';
 import { HomePage } from './home-page';
 import { CategoriesPageComponent } from './pages/categories-page/categories-page.component';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
 
 export default [
     {
@@ -10,6 +11,7 @@ export default [
         children: [
             { path: '', component: HomePage },
             { path: 'categories', component: CategoriesPageComponent },
+            { path: 'products', component: ProductsPageComponent },
             { path: '**', redirectTo: '/' }
         ]
     }
